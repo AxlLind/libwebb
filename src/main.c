8f23fcdbf82e5e82a5598ce0653e3f91335c7ded
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-04-08 11:28:24 +0200

    Add basic argument parsing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "server.h"
 #include "http.h"
 
@@ -17,10 +18,39 @@ char* http_handler(HttpRequest *req) {
   return res;
 }
 
-int main(void) {
+int print_usage(const char *program, int with_description) {
+  FILE *out = with_description ? stdout : stderr;
+  fprintf(out, "usage: %s [-h] [PORT]\n", program);
+  if (with_description) {
+    fprintf(out, "web.c - A small http server written in C\n");
+    fprintf(out, "\n");
+    fprintf(out, "args:\n");
+    fprintf(out, "  PORT  Port to listen on, default " DEFAULT_PORT "\n");
+    fprintf(out, "  -h    Show this help text\n");
+  }
+  return with_description ? 0 : 1;
+}
+
+int main(int argc, char *argv[]) {
+  int opt;
+  while ((opt = getopt(argc, argv, "h")) != -1) {
+    switch (opt) {
+    case 'h':
+      return print_usage(argv[0], 1);
+    default:
+      return print_usage(argv[0], 0);
+    }
+  }
+  if (optind + 1 < argc) {
+    fprintf(stderr, "Unexpected extra positional arguments.\n");
+    return print_usage(argv[0], 0);
+  }
+
+  char *port = optind + 1 == argc ? argv[optind] : DEFAULT_PORT;
+
   HttpServer server;
-  if (!http_server_init(&server, DEFAULT_PORT))
+  if (!http_server_init(&server, port))
     return 1;
-  printf("Server listening on port %s...\n", DEFAULT_PORT);
+  printf("Server listening on port %s...\n", port);
   return http_server_run(&server, http_handler);
 }
