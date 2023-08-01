56ef4886d855f3646f5a2f4a5bccadc8fc27d4e4
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-08-01 08:20:07 +0200

    Add test for positive http parsing

--- tests/test_http.c
+++ tests/test_http.c
@@ -51,9 +51,32 @@ TEST(test_http_conn_next) {
   ASSERT_EQ(str_conn_close(&conn), 0);
 }
 
+TEST(test_http_parse) {
+  const char *request =
+    "GET /test/a.txt?abc=2 HTTP/1.1\r\n"
+    "Host: localhost:8080\r\n"
+    "User-Agent: curl/7.77.0\r\n"
+    "Accept: */*\r\n"
+    "\r\n";
+  StrConnection conn;
+  ASSERT_EQ(str_conn_open(&conn, request), 0);
+
+  HttpRequest req = {0};
+  ASSERT_EQ(http_parse_req(&conn.c, &req), 0);
+  EXPECT_EQ(req.method, HTTP_GET);
+  EXPECT_EQ_STR(req.uri, "/test/a.txt");
+  EXPECT_EQ_STR(req.query, "abc=2");
+  EXPECT_EQ_STR(http_get_header(req.headers, "host"), "localhost:8080");
+  EXPECT_EQ_STR(http_get_header(req.headers, "user-agent"), "curl/7.77.0");
+  EXPECT_EQ_STR(http_get_header(req.headers, "accept"), "*/*");
+  http_req_free(&req);
+
+  ASSERT_EQ(str_conn_close(&conn), 0);
+}
+
 TEST(test_if_one_is_one) {
   EXPECT_EQ(1, 1);
   EXPECT_EQ(1, 1);
 }
 
-TEST_MAIN(test_http_conn_next, test_if_one_is_one)
+TEST_MAIN(test_http_conn_next, test_http_parse, test_if_one_is_one)
