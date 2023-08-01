44fd0d155748168dcd405b43bdc186c1b4bff20d
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-08-01 08:19:10 +0200

    Simplify StrConnection code using macro

--- tests/test_http.c
+++ tests/test_http.c
@@ -9,40 +9,33 @@
 
 #define TMP_NAME "/tmp/webc-XXXXXX"
 
+#define CHECK(ok, msg) \
+  do {                 \
+    if (!(ok)) {       \
+      perror(msg);     \
+      return 1;        \
+    }                  \
+  } while (0)
+
 typedef struct {
   HttpConnection c;
   char path[sizeof(TMP_NAME)];
 } StrConnection;
 
-int str_conn_open(StrConnection *conn, char *s) {
+int str_conn_open(StrConnection *conn, const char *s) {
   memset(conn, 0, sizeof(*conn));
   memcpy(conn->path, TMP_NAME, sizeof(TMP_NAME));
-  conn->c.fd = mkstemp(conn->path);
-  if (conn->c.fd == -1) {
-    perror("failed to open tmpfile");
-    return 1;
-  }
   ssize_t len = (ssize_t) strlen(s);
-  if (write(conn->c.fd, s, len) != len) {
-    perror("failed to write to tmpfile");
-    return 1;
-  }
-  if (lseek(conn->c.fd, 0, SEEK_SET) == -1) {
-    perror("failed to lseek tmpfile");
-    return 1;
-  }
+  conn->c.fd = mkstemp(conn->path);
+  CHECK(conn->c.fd != -1, "failed to open tmpfile");
+  CHECK(write(conn->c.fd, s, len) == len, "failed to write to tmpfile");
+  CHECK(lseek(conn->c.fd, 0, SEEK_SET) != -1, "failed to lseek tmpfile");
   return 0;
 }
 
 int str_conn_close(StrConnection *conn) {
-  if (unlink(conn->path) == -1) {
-    perror("failed to unlink tmpfile");
-    return 1;
-  }
-  if (close(conn->c.fd) == -1) {
-    perror("failed to close tmpfile");
-    return 1;
-  }
+  CHECK(unlink(conn->path) != -1, "failed to unlink tmpfile");
+  CHECK(close(conn->c.fd) != -1, "failed to close tmpfile");
   return 0;
 }
 
