70ff622dbf4cc27618f5f79f9e0bed80445f21f4
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-08-13 12:53:44 +0200

    Simplify assert macros in libtest.h

--- tests/libtest.h
+++ tests/libtest.h
@@ -7,44 +7,36 @@
 
 #define FILENAME (strrchr("/" __FILE__, '/') + 1)
 
-#define ERROR(assert, fmt, ...)                                                   \
-  do {                                                                            \
-    (void) fprintf(stderr, "%s:%d - " fmt "\n", FILENAME, __LINE__, __VA_ARGS__); \
-    *test_failed = 1;                                                             \
-    if (assert)                                                                   \
-      return;                                                                     \
+#define INTERNAL_ASSERT(assert, expr, fmt, ...)                                               \
+  do {                                                                                        \
+    if (!(expr)) {                                                                            \
+      (void) fprintf(stderr, "%s:%d - expected: " fmt "\n", FILENAME, __LINE__, __VA_ARGS__); \
+      *test_failed = 1;                                                                       \
+      if (assert)                                                                             \
+        return;                                                                               \
+    }                                                                                         \
   } while (0)
 
-#define INTERNAL_CMP(a, op, b, assert)                  \
-  do {                                                  \
-    if (!((a) op(b)))                                   \
-      ERROR(assert, "expected: %s " #op " %s", #a, #b); \
-  } while (0)
+#define EXPECT_EQ(a, b) INTERNAL_ASSERT(0, (a) == (b), "%s == %s", #a, #b)
+#define ASSERT_EQ(a, b) INTERNAL_ASSERT(1, (a) == (b), "%s == %s", #a, #b)
 
-#define EXPECT_EQ(a, b) INTERNAL_CMP(a, ==, b, 0)
-#define ASSERT_EQ(a, b) INTERNAL_CMP(a, ==, b, 1)
+#define EXPECT_NE(a, b) INTERNAL_ASSERT(0, (a) != (b), "%s != %s", #a, #b)
+#define ASSERT_NE(a, b) INTERNAL_ASSERT(1, (a) != (b), "%s != %s", #a, #b)
 
-#define EXPECT_NE(a, b) INTERNAL_CMP(a, !=, b, 0)
-#define ASSERT_NE(a, b) INTERNAL_CMP(a, !=, b, 1)
+#define EXPECT_LT(a, b) INTERNAL_ASSERT(0, (a) < (b), "%s < %s", #a, #b)
+#define ASSERT_LT(a, b) INTERNAL_ASSERT(1, (a) < (b), "%s < %s", #a, #b)
 
-#define EXPECT_LT(a, b) INTERNAL_CMP(a, <, b, 0)
-#define ASSERT_LT(a, b) INTERNAL_CMP(a, <, b, 1)
+#define EXPECT_LE(a, b) INTERNAL_ASSERT(0, (a) <= (b), "%s <= %s", #a, #b)
+#define ASSERT_LE(a, b) INTERNAL_ASSERT(1, (a) <= (b), "%s <= %s", #a, #b)
 
-#define EXPECT_LE(a, b) INTERNAL_CMP(a, <=, b, 0)
-#define ASSERT_LE(a, b) INTERNAL_CMP(a, <=, b, 1)
+#define EXPECT_GT(a, b) INTERNAL_ASSERT(0, (a) > (b), "%s > %s", #a, #b)
+#define ASSERT_GT(a, b) INTERNAL_ASSERT(1, (a) > (b), "%s > %s", #a, #b)
 
-#define EXPECT_GT(a, b) INTERNAL_CMP(a, >, b, 0)
-#define ASSERT_GT(a, b) INTERNAL_CMP(a, >, b, 1)
+#define EXPECT_GE(a, b) INTERNAL_ASSERT(0, (a) >= (b), "%s >= %s", #a, #b)
+#define ASSERT_GE(a, b) INTERNAL_ASSERT(1, (a) >= (b), "%s >= %s", #a, #b)
 
-#define EXPECT_GE(a, b) INTERNAL_CMP(a, >=, b, 0)
-#define ASSERT_GE(a, b) INTERNAL_CMP(a, >=, b, 1)
-
-#define EXPECT_EQ_STR(a, b)                           \
-  do {                                                \
-    const char *aa = (a), *bb = (b);                  \
-    if (strcmp(aa, bb) != 0)                          \
-      ERROR(0, "expected: \"%s\" == \"%s\"", aa, bb); \
-  } while (0)
+#define EXPECT_EQ_STR(a, b) INTERNAL_ASSERT(0, strcmp((a), (b)) == 0, "%s == %s", #a, #b);
+#define ASSERT_EQ_STR(a, b) INTERNAL_ASSERT(1, strcmp((a), (b)) == 0, "%s == %s", #a, #b);
 
 typedef struct {
   void (*fn)(int *);
