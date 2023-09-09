780ac13c06b8f9400a8d17bcb66afe1262cb3de9
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-09-09 10:50:10 +0200

    Remove unnecessary error printing

--- bin/webb.c
+++ bin/webb.c
@@ -78,10 +78,8 @@ int http_handler(const WebbRequest *req, WebbResponse *res) {
   char input_path[PATH_MAX], file[PATH_MAX];
   if (snprintf(input_path, PATH_MAX, "%s%s", WORK_DIR, req->uri) < 0)
     return 404;
-  if (realpath(input_path, file) == NULL) {
-    perror("realpath");
+  if (realpath(input_path, file) == NULL)
     return 404;
-  }
   // check path traversal attack
   if (strstr(file, WORK_DIR) != file)
     return 404;
